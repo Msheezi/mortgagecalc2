38dc5a295e128fef1aaeffba5a4d541fd97529e1
msheezi <marshall.randolph@gmail.com>
2021-01-02 13:48:19 -0800

    Update calcs to include comparison for extra payments

--- src/components/calculatorFunc.js
+++ src/components/calculatorFunc.js
@@ -22,19 +22,21 @@ For example, a 30-year fixed mortgage would have 360 payments (30x12=360).
  export const calc = ({loanAmt, interestRate, loanTerm, extraPayment = 0}) => {
     let payments = []
     let balance = currency(loanAmt).value
+    let scheduledBalance = currency(loanAmt).value
     let extra = extraPayment * 1
-    console.log(balance, extra)
+    // console.log(balance, extra)
 
     let r = ((interestRate/100) /12)
     // let i = (balance * r * Math.pow(1 + r, loanTerm))
     let v = r * Math.pow(1 + r, loanTerm)
     let i = currency(balance).multiply(v) 
     let j = Math.pow(1 + r,  loanTerm) - 1
-    console.log(balance, i.value)
+    // console.log(balance, i.value)
     let payment = currency(i).divide(j)
-    console.log(payment)
+    // console.log(payment)
 
     let totalInterest = 0
+    let scheduledInterest = 0
     
     /**
      * calculate interest savings with extra payments
@@ -45,19 +47,50 @@ For example, a 30-year fixed mortgage would have 360 payments (30x12=360).
      */
     
 
+    const calcScheduled = (payment) => {
+        let checker2 = payment.value 
+        let interest
+        while (scheduledBalance > 0){
+
+            if (checker2 < scheduledBalance){
+                
+                interest = currency(scheduledBalance).multiply(r)
+                scheduledInterest += interest.value 
+                let princePayment = payment.subtract(interest)
+                scheduledBalance = currency(scheduledBalance).subtract(princePayment).value
+            } else {
+                interest = currency(scheduledBalance).multiply(r)
+                scheduledInterest += interest.value 
+                payment = scheduledBalance
+                scheduledBalance = 0
+                
+            }
+            
+        }
+    }
+
+    calcScheduled(payment)
+
+
     while (balance > 0){
         let checker = payment.value
         if ( checker < balance){
-        let ttlPayment = payment.add(extra)
+
+        let scheduledPayment = payment
+        let extraPayment = payment.add(extra)
+        console.log(scheduledPayment.value, extraPayment.value)
+
         let interestPaid = currency(balance).multiply(r)
             totalInterest += interestPaid.value
-        let principlePaid = ttlPayment.subtract(interestPaid)
+        let principlePaid = extraPayment.subtract(interestPaid)
+        
             balance = currency(balance).subtract(principlePaid).value
-           console.log()     
+
+        //    console.log()     
               
         let paymentObj = {
             
-            ttlPayment: ttlPayment.value.toLocaleString('en-US', {style: "currency", currency: "USD"}) ,
+            ttlPayment: extraPayment.value.toLocaleString('en-US', {style: "currency", currency: "USD"}) ,
             interestPaid: interestPaid.value.toLocaleString('en-US', {style: "currency", currency: "USD"}),  
             principlePaid: principlePaid.value.toLocaleString('en-US', {style: "currency", currency: "USD"}), 
             extraPayment: extra,
@@ -82,30 +115,16 @@ For example, a 30-year fixed mortgage would have 360 payments (30x12=360).
         }
         payments.push(paymentObj)
         }
-        console.log(totalInterest)
+        // console.log(totalInterest)
     }
 
+    console.log(scheduledInterest, totalInterest)
+    /**
+     * return an object paymentdata = {
+     * payments:[], scheduledInterest, totalInterest
+     * }
+     */
     return payments
-    // while (loanAmt > 0){
-    //     // if (payment < loanAmt){
-    //         let ttlPayment = (payment + (extraPayment * 1)).toFixed(2) * 1
-    //     let interestPaid = (loanAmt * r ).toFixed(2) * 1
-    //     let principlePaid = (payment + extraPayment - interestPaid).toFixed(2) * 1
-    //         loanAmt = (loanAmt - (principlePaid+extraPayment)).toFixed(2) * 1
-    //     let paymentObj = {
-            
-    //         ttlPayment, 
-    //         interestPaid, 
-    //         principlePaid, 
-    //         extraPayment,
-    //         remainingPrinciple: loanAmt
-    //     }
-    //     payments.push(paymentObj)}
-    // //     else {
-    // //         payment = loanAmt
-    // //     }
-    // // }
-
-    // return payments
+   
 }
  
